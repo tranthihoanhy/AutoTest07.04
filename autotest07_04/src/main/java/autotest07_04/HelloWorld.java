e0237adde17facff8b2f5f71cab630c9a954a097
unknown <tranthihoanhy@gmail.com>
2024-05-12 21:26:52 +0700

    add hello java file

---
+++ autotest07_04/src/main/java/autotest07_04/HelloWorld.java
@@ -0,0 +1,10 @@
+package autotest07_04;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello day 6");
+	}
+
+}
